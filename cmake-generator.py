dbac0e73fb8adb27755bf32f42dfe3407664215c
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-04-30 09:15:01 -0600

    Update cmake-generator.py

--- cmake-generator.py
+++ cmake-generator.py
@@ -4,41 +4,36 @@ class CMakeScriptGenerator:
     def __init__(self, project_name='YourProject'):
         self.project_name = project_name
 
-    def generate_source_build_script(self):
-        source_script = f"""
-cmake_minimum_required(VERSION 3.0)
+    def generate_root_cmake_file(self):
+        root_script = f"""
+cmake_minimum_required(VERSION 3.10)
 project({self.project_name} LANGUAGES CXX)
 
 add_subdirectory(solutions)
-        """
+"""
         with open('CMakeLists.txt', 'w') as file:
-            file.write(source_script)
+            file.write(root_script)
 
-    def generate_subdirectory_build_script(self):
+    def generate_subdirectory_cmake_files(self):
         solutions_dir = 'solutions'
         solutions = [os.path.splitext(f)[0] for f in os.listdir(solutions_dir) if f.endswith('.cpp')]
         solved = len(solutions)
     
         subdirectory_script = f"""
-set(solutions {solutions})
-set(solved {solved})
+set(solutions
+    {";\n    ".join(solutions)}
+)
 
-if(solved >= 10)
-    set(index_str "_0")
-else()
-    set(index_str "_0[0:1]")
-endif()
-
-foreach(iter RANGE ${solved})
-    list(GET solutions ${iter} name)
-    add_executable(prog-${{index_str}}${{iter+1}} ${name}.cpp)
+foreach(solution ${{solutions}})
+    add_executable(${{solution}} ${{solution}}.cpp)
 endforeach()
-        """
+"""
+        os.makedirs(solutions_dir, exist_ok=True)
         with open(os.path.join(solutions_dir, 'CMakeLists.txt'), 'w') as file:
             file.write(subdirectory_script)
 
 
 if __name__ == "__main__":
     cmake_generator = CMakeScriptGenerator(project_name='Hacker Rank')
-    cmake_generator.generate_source_build_script()
-    cmake_generator.generate_subdirectory_build_script()
+    cmake_generator.generate_root_cmake_file()
+    cmake_generator.generate_subdirectory_cmake_files()
